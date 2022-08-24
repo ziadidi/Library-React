dc3e8d5871f978ce8c420426f5bf51ea45ce2c81
ziadidi <dianepucci19@gmail.com>
2022-08-24 18:29:01 +0100

    new

--- library/src/App.jsx
+++ library/src/App.jsx
@@ -1,17 +1,37 @@
+import React, { useEffect, useState } from "react";
 import Nav from "./components/Nav";
 import Footer from "./components/Footer";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Books from "./pages/Books";
 import { books } from "./data"
+import BookInfo from "./pages/BookInfo";
+import Cart from "./pages/Cart";
 
 function App() {
+  const [cart, setCart] = useState([]);
+
+  function addToCart(book){
+    const dupeItem = cart.find(item => +item.id === +book.id)
+    if (dupeItem){
+      dupeItem.quantity += 1;
+    }
+    console.log(dupeItem)
+    setCart([...cart, {...book, quantity: 1}])
+  }
+
+  useEffect(() => {
+    // console.log(cart)
+  }, [cart])
+
   return (
     <Router>
       <div className="App">
         <Nav />
       <Route path="/" exact component={Home} />
-      <Route path="/books" render={() => <Books books={books} /> } />
+      <Route path="/books" exact render={() => <Books books={books} /> } />
+      <Route path="/books/:id" render={() => <BookInfo books={books} addToCart={addToCart} /> } />
+      <Route path="/cart" render={() => <Cart books={books} /> } />
         <Footer />
       </div>
     </Router>
